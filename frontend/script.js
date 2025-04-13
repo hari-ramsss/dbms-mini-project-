2a99819664daeeff98867a7ef208586c1ef87fdc
unknown <hariram.27csa@licet.ac.in>
2025-04-13 19:49:43 +0530

    dbms creation

---
+++ frontend/script.js
@@ -0,0 +1,6 @@
+function showPage(pageId) {
+    const pages = document.querySelectorAll('.page');
+    pages.forEach(p => p.classList.remove('active'));
+    document.getElementById(pageId).classList.add('active');
+  }
+  
